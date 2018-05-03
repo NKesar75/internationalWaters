22952ba985faeb24db9d93e487cb310b52fda7de
Unknown <rpchandan@fullsail.edu>
2018-05-03 10:42:24 -0400

    bugs fixed for location permition for update

--- app/src/main/java/domain/internationalwaters/internationalwaters/Home.java
+++ app/src/main/java/domain/internationalwaters/internationalwaters/Home.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -34,6 +35,7 @@ public class Home extends AppCompatActivity {
     private TextView walklabel;
     private TextView planelabel;
     private TextView timeTaken;
+    private Location location;
     static final int REQUEST_LOCATION = 1;
     LocationManager locationManager;
     Double lon;
@@ -62,48 +64,54 @@ public class Home extends AppCompatActivity {
             public void onClick(View view) {
 
                 getLocation();
-                Double wintlat = 37.139943;
-                Double wintlon = -152.201824;
+                if(location != null)
+                {
+                    Double wintlat = 37.139943;
+                    Double wintlon = -152.201824;
 
-                Double eintlat = 35.868339 ;
-                Double eintlon = -38.471365;
+                    Double eintlat = 35.868339;
+                    Double eintlon = -38.471365;
 
-                double flat3 = ((wintlat - lat) * (Math.PI / 180.0f));
-                double flon3 = ((wintlon - lon) * (Math.PI / 180.0f));
+                    double flat3 = ((wintlat - lat) * (Math.PI / 180.0f));
+                    double flon3 = ((wintlon - lon) * (Math.PI / 180.0f));
 
-                double x = sin(flat3 / 2.0f) * sin(flat3 / 2.0f) + cos((lat) * (0.01745329251)) * cos((wintlat) * (0.01745329251)) * sin(flon3 / 2.0f) * sin(flon3 / 2.0f);
-                double west = (20903520.0f * (2.0f * atan2(sqrt(x), sqrt(1.0f - x))));
-                west = west * 0.000189394;
+                    double x = sin(flat3 / 2.0f) * sin(flat3 / 2.0f) + cos((lat) * (0.01745329251)) * cos((wintlat) * (0.01745329251)) * sin(flon3 / 2.0f) * sin(flon3 / 2.0f);
+                    double west = (20903520.0f * (2.0f * atan2(sqrt(x), sqrt(1.0f - x))));
+                    west = west * 0.000189394;
 
-                 double West =  roundTwoDecimals(west);
-
-
-                 flat3 = ((eintlat - lat) * (Math.PI / 180.0f));
-                 flon3 = ((eintlon - lon) * (Math.PI / 180.0f));
-
-                x = sin(flat3 / 2.0f) * sin(flat3 / 2.0f) + cos((lat) * (0.01745329251)) * cos((eintlat) * (0.01745329251)) * sin(flon3 / 2.0f) * sin(flon3 / 2.0f);
-
-                double east = (20903520.0f * (2.0f * atan2(sqrt(x), sqrt(1.0f - x))));
-                east = east * 0.000189394;
+                    double West = roundTwoDecimals(west);
 
 
-                double East =  roundTwoDecimals(east);
+                    flat3 = ((eintlat - lat) * (Math.PI / 180.0f));
+                    flon3 = ((eintlon - lon) * (Math.PI / 180.0f));
+
+                    x = sin(flat3 / 2.0f) * sin(flat3 / 2.0f) + cos((lat) * (0.01745329251)) * cos((eintlat) * (0.01745329251)) * sin(flon3 / 2.0f) * sin(flon3 / 2.0f);
+
+                    double east = (20903520.0f * (2.0f * atan2(sqrt(x), sqrt(1.0f - x))));
+                    east = east * 0.000189394;
 
 
-                if (east > west){
-                    distancelabel.setText("Distance: " + West + " Miles");
-                    timeTaken.setText("Approximate time to reach:");
-                    walklabel.setText("Walk: " + roundTwoDecimals(West/3) + " Hours");
-                    carlabel.setText("Car: " + roundTwoDecimals(West/70) + " Hours");
-                    planelabel.setText("Plane: " + roundTwoDecimals(West/575)+ " Hours");
-                }else{
-                    distancelabel.setText("Distance: " + East + " Miles");
-                    timeTaken.setText("Approximate time to reach:");
-                    walklabel.setText("Walk: " + roundTwoDecimals(East/3) + " Hours");
-                    carlabel.setText("Car: " + roundTwoDecimals(East/70) + " Hours");
-                    planelabel.setText("Plane: " + roundTwoDecimals(East/575) + " Hours");
+                    double East = roundTwoDecimals(east);
+
+
+                    if (east > west) {
+                        distancelabel.setText("Distance: " + West + " Miles");
+                        timeTaken.setText("Approximate time to reach:");
+                        walklabel.setText("Walk: " + roundTwoDecimals(West / 3) + " Hours");
+                        carlabel.setText("Car: " + roundTwoDecimals(West / 70) + " Hours");
+                        planelabel.setText("Plane: " + roundTwoDecimals(West / 575) + " Hours");
+                    } else {
+                        distancelabel.setText("Distance: " + East + " Miles");
+                        timeTaken.setText("Approximate time to reach:");
+                        walklabel.setText("Walk: " + roundTwoDecimals(East / 3) + " Hours");
+                        carlabel.setText("Car: " + roundTwoDecimals(East / 70) + " Hours");
+                        planelabel.setText("Plane: " + roundTwoDecimals(East / 575) + " Hours");
+                    }
                 }
-
+                else
+                {
+                    Toast.makeText(Home.this, "Please Enable Your Location", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
@@ -121,12 +129,14 @@ public class Home extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
         } else {
-            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
             if (location != null) {
                 lat = location.getLatitude();
                 lon = location.getLongitude();
-
-
+            }
+            else
+            {
+                Toast.makeText(Home.this, "Please Enable Your Location", Toast.LENGTH_SHORT).show();
             }
         }
     }
